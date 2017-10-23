83a00b5a453e1b9fb7d9b2daad3780de8e06f1e8
lrz <liuruzheng@100doc.cn>
2017-10-23 10:29:39 +0800

    no message

--- MRCommon/MRCommonPackaging/MRCommonColorAndPicture.m
+++ MRCommon/MRCommonPackaging/MRCommonColorAndPicture.m
@@ -294,6 +294,20 @@
     return image;
 }
 
++ (UIImage *)ct_imageFromImage:(UIImage *)image inRect:(CGRect)rect{
+    
+    //把像 素rect 转化为 点rect（如无转化则按原图像素取部分图片）
+    CGFloat scale = [UIScreen mainScreen].scale;
+    CGFloat x= rect.origin.x*scale,y=rect.origin.y*scale,w=rect.size.width*scale,h=rect.size.height*scale;
+    CGRect dianRect = CGRectMake(x, y, w, h);
+    
+    //截取部分图片并生成新图片
+    CGImageRef sourceImageRef = [image CGImage];
+    CGImageRef newImageRef = CGImageCreateWithImageInRect(sourceImageRef, dianRect);
+    UIImage *newImage = [UIImage imageWithCGImage:newImageRef scale:[UIScreen mainScreen].scale orientation:UIImageOrientationUp];
+    return newImage;
+}
+
 /**
  拍照
  */
@@ -446,8 +460,6 @@
     UIGraphicsEndImageContext();
     return newImage;
 }
-
-
 
 
 
