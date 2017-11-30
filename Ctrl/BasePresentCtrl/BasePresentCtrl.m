7951d4265695b1211a215fdcceeb8d94b248c657
lrz <liuruzheng@100doc.cn>
2017-11-30 10:10:36 +0800

    适配 iPhoneX

--- Ctrl/BasePresentCtrl/BasePresentCtrl.m
+++ Ctrl/BasePresentCtrl/BasePresentCtrl.m
@@ -14,6 +14,15 @@
 
 #define kScreenWidth [UIScreen mainScreen].bounds.size.width
 #define kScreenHeight [UIScreen mainScreen].bounds.size.height
+#define kIs_iPhone (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+// 导航栏
+#define kNavigationHeight   (kIs_iPhone_X ? (kIsVerticalScreen? 88 : 32) : (kIsVerticalScreen? 64 : 32))
+
+/// 是否是竖屏
+#define kIsVerticalScreen (kScreenHeight > kScreenWidth)
+/// 真实屏幕高
+#define kScreenRealHeight (MAX(kScreenWidth, kScreenHeight))
+#define kIs_iPhone_X (kIs_iPhone && kScreenRealHeight == 812.0)
 
 @interface BasePresentCtrl ()
 
@@ -68,7 +77,7 @@
     
     if(self.isNotLandscape){    // 不能横屏
         CGFloat width = [UIScreen mainScreen].bounds.size.width;
-        _navView.frame = CGRectMake(0, 0, width, 64);
+        _navView.frame = CGRectMake(0, 0, width, kNavigationHeight);
         _sendButton.frame = CGRectMake(width - kButtonWidth - 15, 20 + 7, kButtonWidth, kButtonHeight);
         _cancelButton.frame = CGRectMake(15, 20 + 7, kButtonWidth, kButtonHeight);
         _line.frame = CGRectMake(0, 64 - .5, _navView.mr_width, .5);
@@ -78,29 +87,24 @@
     }
     
     CGFloat width = [UIScreen mainScreen].bounds.size.width;
-    if ([[UIDevice currentDevice] orientation] == UIInterfaceOrientationPortrait
-        || [[UIDevice currentDevice] orientation] == UIInterfaceOrientationPortraitUpsideDown) {
-        // 竖屏
+    /// 横屏
+    if([UIDevice currentDevice].orientation == UIDeviceOrientationLandscapeLeft || [UIDevice currentDevice].orientation == UIDeviceOrientationLandscapeRight){
         [UIView animateWithDuration:.5 animations:^{
-            
-            _navView.frame = CGRectMake(0, 0, width, 64);
-            _sendButton.frame = CGRectMake(width - kButtonWidth - 15, 20 + 7, kButtonWidth, kButtonHeight);
-            _cancelButton.frame = CGRectMake(15, 20 + 7, kButtonWidth, kButtonHeight);
-            _line.frame = CGRectMake(0, 64 - .5, _navView.mr_width, .5);
-            self.navigationLabel.mr_centerX = self.view.mr_centerX;
-            self.navigationLabel.mr_y = 20 + 44 / 2 - self.navigationLabel.mr_height / 2;
-        }];
-    } else {
-
-        // 横屏
-        [UIView animateWithDuration:.5 animations:^{
-            
             _navView.frame = CGRectMake(0, 0, width, 44);
             _sendButton.frame = CGRectMake(width - kButtonWidth - 15, 7, kButtonWidth, kButtonHeight);
             _cancelButton.frame = CGRectMake(15, 7, kButtonWidth, kButtonHeight);
             _line.frame = CGRectMake(0, 44 - .5, _navView.mr_width, .5);
             self.navigationLabel.mr_centerX = self.view.mr_centerX;
             self.navigationLabel.mr_y = 44 / 2 - self.navigationLabel.mr_height / 2;
+        }];
+    }else{          /// 竖屏
+        [UIView animateWithDuration:.5 animations:^{
+            _navView.frame = CGRectMake(0, 0, width, kNavigationHeight);
+            _sendButton.frame = CGRectMake(width - kButtonWidth - 15, 20 + 7, kButtonWidth, kButtonHeight);
+            _cancelButton.frame = CGRectMake(15, 20 + 7, kButtonWidth, kButtonHeight);
+            _line.frame = CGRectMake(0, 64 - .5, _navView.mr_width, .5);
+            self.navigationLabel.mr_centerX = self.view.mr_centerX;
+            self.navigationLabel.mr_y = 20 + 44 / 2 - self.navigationLabel.mr_height / 2;
         }];
     }
 }
@@ -124,7 +128,7 @@
     
     if(!_navView){
         
-        _navView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 64)];
+        _navView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, kNavigationHeight)];
         [self.view addSubview:_navView];
         
         self.line = [[UIView alloc] initWithFrame:CGRectMake(0, _navView.mr_height, kScreenWidth, .5)];
