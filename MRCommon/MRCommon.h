185c8b9276f7f5b350c22a9690ed43d00c5d26a9
MixReality <17194038777@163.com>
2018-05-03 10:45:08 +0800

    2018.05.03 剽窃 SDWebImage 的宏

--- MRCommon/MRCommon.h
+++ MRCommon/MRCommon.h
@@ -75,6 +75,20 @@
 /// 解锁
 #define kUnlock(lock) dispatch_semaphore_signal(lock);
 
+/// 子线程回到主线程
+#ifndef dispatch_queue_async_safe
+#define dispatch_queue_async_safe(queue, block)\
+if (strcmp(dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL), dispatch_queue_get_label(queue)) == 0) {\
+block();\
+} else {\
+dispatch_async(queue, block);\
+}
+#endif
+
+#ifndef dispatch_main_async_safe
+#define dispatch_main_async_safe(block) dispatch_queue_async_safe(dispatch_get_main_queue(), block)
+#endif
+
 /// userdefaults
 #define kUserDefaults                 [NSUserDefaults standardUserDefaults]
 #define kStoryBoard(format)       [UIStoryboard storyboardWithName:format bundle:nil].instantiateInitialViewController
